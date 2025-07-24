3ec43afd849855e302ba91bafc310b08793051a9
varenya <varenyajj@gmail.com>
2025-07-24 15:29:50 +0200

    ruff format .

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,5 +1,8 @@
 from P6.sample import *
 
+
 def test_sample():
-    print("'There's four legs on a table. Four walls on a house. And four seats in a Mustang GT. We're the Four Corpsmen. You don't screw with what works.'")
+    print(
+        "'There's four legs on a table. Four walls on a house. And four seats in a Mustang GT. We're the Four Corpsmen. You don't screw with what works.'"
+    )
     assert True

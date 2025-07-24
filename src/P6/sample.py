58d44c8012bd3cbdd36dc995514a15e30b8637d4
varenya <varenyajj@gmail.com>
2025-07-24 13:54:49 +0200

    Sample file and test
    Signed-off-by: varenya <varenyajj@gmail.com>

---
+++ src/P6/sample.py
@@ -0,0 +1,4 @@
+def sample():
+    print("Hello World :)")
+    print("We're not in Kansas anymore... See you on the other side!")
+    return True
---
+++ tests/test_sample.py
@@ -0,0 +1,5 @@
+from P6.sample import *
+
+def test_sample():
+    print("'There's four legs on a table. Four walls on a house. And four seats in a Mustang GT. We're the Four Corpsmen. You don't screw with what works.'")
+    assert True
